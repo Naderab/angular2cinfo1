94521a173f4ae319f749b01c9fcf3d4cdc88fdc7
Naderab <abdellaouinader@gmail.com>
2022-11-10 20:27:01 +0100

    manipulation de composants

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -14,9 +14,20 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = [
-      {id: 1, title: "T-shirt 1", price: 18, quantity: 0, picture: ""},
-      {id: 2, title: "T-shirt 2", price: 21, quantity: 10, picture: ""},
-      {id: 3, title: "T-shirt 3", price: 16, quantity: 8, picture: ""}, ]
+      {id: 1, title: "T-shirt 1", price: 18, quantity: 0, picture: "https://static.bershka.net/4/photos2/2022/I/0/2/p/2766/777/800//01/2766777800_2_4_3.jpg?t=1660741431395",like:0},
+      {id: 2, title: "T-shirt 2", price: 21, quantity: 10, picture: "https://static.bershka.net/4/photos2/2022/I/0/2/p/2766/777/800//01/2766777800_2_4_3.jpg?t=1660741431395",like:0},
+      {id: 3, title: "T-shirt 3", price: 16, quantity: 8, picture: "https://static.bershka.net/4/photos2/2022/I/0/2/p/2766/777/800//01/2766777800_2_4_3.jpg?t=1660741431395",like:0}, ]
+  }
+
+  BuyProduct(id:number){
+    for(let i=0;i<this.products.length;i++){
+      if(this.products[i].id == id) {
+        this.products[i].quantity--
+      }
+    }
+  }
+  LikeProduct(yyyy:number){
+    this.products.map((x)=>x.id == yyyy && x.like++)
   }
 
 }
